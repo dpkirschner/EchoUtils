bea878c8b52b075832e9c584bd3eebc9d5bad95b
Daniel Kirschner <dpkirschner@gmail.com>
2015-12-01 17:15:47 -0800

    refactoring to use builder pattern instead of multiple constructors

--- src/main/java/com/dank/echo/utils/ResponseFactory.java
+++ src/main/java/com/dank/echo/utils/ResponseFactory.java
@@ -1,145 +1,60 @@
 package com.dank.echo.utils;
 
-import lombok.NonNull;
-
 import org.apache.commons.lang3.StringUtils;
 
 import com.amazon.speech.speechlet.SpeechletResponse;
-import com.amazon.speech.ui.Card;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 import com.google.common.base.Preconditions;
 
 public class ResponseFactory {
-	/*TODO: Refactor to use a token POJO as input instead of directly using strings
-	 * This will reduce the amount of repetition and also make the function much more clear
-	 */
-	
 	private ResponseFactory(){};
 
-	public static Reprompt createReprompt(@NonNull PlainTextOutputSpeech speechOutput) {	
+	private static Reprompt createReprompt(ResponseFactoryToken token) {
+		Preconditions.checkArgument(!StringUtils.isEmpty(token.getRepromptSpeech()), "Reprompt Speech must not be empty");
+		
+		PlainTextOutputSpeech speechOutput = new PlainTextOutputSpeech();
+		speechOutput.setText(token.getRepromptSpeech());
+        
 		Reprompt reprompt = new Reprompt();
         reprompt.setOutputSpeech(speechOutput);
 		return reprompt;
 	}
 	
-	public static PlainTextOutputSpeech createSpeech(String speechText) {
-		validateStrings(speechText);
+	private static PlainTextOutputSpeech createSpeech(ResponseFactoryToken token) {
+		Preconditions.checkArgument(!StringUtils.isEmpty(token.getOutputSpeech()), "Output Speech must not be empty");
 		
 		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
+        speech.setText(token.getOutputSpeech());
 		return speech;
 	}
 
-	public static SimpleCard createCard(String title, String content) {
-		validateStrings(title, content);
+	private static SimpleCard createCard(ResponseFactoryToken token) {
+		if(StringUtils.isEmpty(token.getCardTitle()) || StringUtils.isEmpty(token.getCardContent())) {
+			return null;
+		}
 		
         SimpleCard card = new SimpleCard();
-        card.setTitle(title);
-        card.setContent(content);
+        card.setTitle(token.getCardTitle());
+        card.setContent(token.getCardContent());
 		return card;
 	}
-
-    /**
-     * Wrapper for creating the Tell response. The OutputSpeech and {@link Card} objects are
-     * created from the input strings.
-     *
-     * @param speechText
-     *            the output to be spoken
-     * @param cardTitle
-     *            the title of the card to be returned
-     * @param cardContent
-     * 			  the content the card should contain
-     * @return SpeechletResponse the speechlet response
-     */
-    public static SpeechletResponse tellResponse(String speechText, String cardTitle, String cardContent) {
-    	validateStrings(speechText, cardTitle, cardContent);
-    	
-    	PlainTextOutputSpeech outputSpeech = createSpeech(speechText);
-    	SimpleCard card = createCard(cardTitle, cardContent);
-        return SpeechletResponse.newTellResponse(outputSpeech, card);
-    }
-    
-    /**
-     * Wrapper for creating the Tell response. The OutputSpeech and {@link Card} objects are
-     * created from the input strings.
-     *
-     * @param speechText
-     *            the output to be spoken
-     * @param cardTitle
-     *            the title of the card to be returned
-     * @param cardContent
-     * 			  the content the card should contain
-     * @return SpeechletResponse the speechlet response
-     */
-    public static SpeechletResponse closeResponse(String speechText, String cardTitle, String cardContent) {
-    	validateStrings(speechText, cardTitle);
-    	
-    	PlainTextOutputSpeech outputSpeech = createSpeech(speechText);
-    	SimpleCard card = createCard(cardTitle, cardContent);
-        
-    	SpeechletResponse response = SpeechletResponse.newTellResponse(outputSpeech, card);
-        response.setShouldEndSession(true);
-        return response;
-    }
-    
-    /**
-     * Wrapper for creating the Ask response. The OutputSpeech and {@link Reprompt} objects are
-     * created from the same input string.
-     *
-     * @param speechText
-     *            the output to be spoken as well as the reprompt if the user doesn't reply or is misunderstood
-     * @return SpeechletResponse the speechlet response
-     */
-    public static SpeechletResponse askResponse(String speechText) {
-    	validateStrings(speechText);
-    	
-    	return askResponse(speechText, speechText);
-    }
 	
-    /**
-     * Wrapper for creating the Ask response. The OutputSpeech and {@link Reprompt} objects are
-     * created from the input strings.
-     *
-     * @param speechText
-     *            the output to be spoken
-     * @param repromptText
-     *            the reprompt for if the user doesn't reply or is misunderstood.
-     * @return SpeechletResponse the speechlet response
-     */
-    public static SpeechletResponse askResponse(String speechText, String repromptText) {
-    	validateStrings(speechText, repromptText);
-    	
-    	PlainTextOutputSpeech outputSpeech = createSpeech(speechText);
-    	Reprompt reprompt = createReprompt(createSpeech(repromptText));
-        return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
-    }
-    
-    /**
-     * Wrapper for creating the Ask response. The OutputSpeech, {@link Reprompt}, and {@link Card} objects are
-     * created from the input strings.
-     *
-     * @param speechText
-     *            the output to be spoken
-     * @param repromptText
-     *            the reprompt for if the user doesn't reply or is misunderstood.
-     * @return SpeechletResponse the speechlet response
-     */
-    public static SpeechletResponse askResponse(String speechText, String repromptText, String cardTitle) {
-    	validateStrings(speechText, repromptText, cardTitle);
-    	
-    	PlainTextOutputSpeech outputSpeech = createSpeech(speechText);
-    	Reprompt reprompt = createReprompt(createSpeech(repromptText));
-        return SpeechletResponse.newAskResponse(outputSpeech, reprompt, createCard(cardTitle, speechText));
-    }
-	
-	/**
-	 * Utility method that consolidates the argument checking for each string passed
-	 */
-	private static void validateStrings(String... texts) {
-		for(String text : texts) {
-			Preconditions.checkArgument(!StringUtils.isEmpty(text), "text must not be empty");
+	public static SpeechletResponse from(ResponseFactoryToken token) {
+		token.validate();
+		SimpleCard card = createCard(token);
+		SpeechletResponse response;
+		if(StringUtils.isEmpty(token.getRepromptSpeech())) {
+			response = (card == null) 
+					? SpeechletResponse.newTellResponse(createSpeech(token)) 
+					: SpeechletResponse.newTellResponse(createSpeech(token), card);
+			response.setShouldEndSession(token.isShouldEndSession());
+		} else {
+			response = (card == null)
+					? SpeechletResponse.newAskResponse(createSpeech(token), createReprompt(token))
+					: SpeechletResponse.newAskResponse(createSpeech(token), createReprompt(token), card);
 		}
+		return response;
 	}
 }
--- src/test/java/com/dank/echo/utils/ResponseFactoryTest.java
+++ src/test/java/com/dank/echo/utils/ResponseFactoryTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.amazon.speech.speechlet.SpeechletResponse;
@@ -19,129 +18,17 @@ public class ResponseFactoryTest {
 	private static String REPROMPT_TEXT = "REPROMPT TEXT";
 	private static String CARD_TITLE = "CARD TITLE";
 	private static String CARD_CONTENT = "CARD TEXT";
-	private static String EMPTY_TEXT = "";
 	
-	@Before
-	public void setup() {
-		
-	}
-
     @Test
-    public void reprompt() {
-    	PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-    	Reprompt reprompt = ResponseFactory.createReprompt(outputSpeech);
-
-        assertEquals(outputSpeech, reprompt.getOutputSpeech());
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void repromptNullSpeech() {
-    	ResponseFactory.createReprompt(null);
-    }
-    
-    @Test
-    public void createSpeech() {
-    	PlainTextOutputSpeech outputSpeech = ResponseFactory.createSpeech(SPEECH_TEXT);
-        
-        assertEquals(SPEECH_TEXT, outputSpeech.getText());
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void createSpeechEmptyText() {
-    	ResponseFactory.createSpeech(EMPTY_TEXT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void createSpeechNulltext() {
-    	ResponseFactory.createSpeech(EMPTY_TEXT);
-    }
-    
-    @Test
-    public void createCard() {
-    	SimpleCard card = ResponseFactory.createCard(CARD_TITLE, CARD_CONTENT);
-    	
-    	assertEquals(CARD_TITLE, card.getTitle());
-        assertEquals(CARD_CONTENT, card.getContent());
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void createCardEmptyTitle() {
-    	ResponseFactory.createCard(EMPTY_TEXT, CARD_CONTENT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void createSpeechEmptyContent() {
-    	ResponseFactory.createCard(CARD_TITLE, EMPTY_TEXT);
-    }
-    
-    @Test
-    public void createTellReponse() {
-    	SpeechletResponse response = ResponseFactory.tellResponse(SPEECH_TEXT, CARD_TITLE, CARD_CONTENT);
+    public void withCloseSession() {
+    	ResponseFactoryToken token = ResponseFactoryToken.builder()
+        		.outputSpeech(SPEECH_TEXT)
+        		.shouldEndSession(true)
+        		.build();
+        SpeechletResponse response = ResponseFactory.from(token);
     	
     	assertTrue(response.getShouldEndSession());
     	assertNull(response.getReprompt());
-    	
-    	assertNotNull(response.getOutputSpeech());
-    	assertEquals(SPEECH_TEXT, ((PlainTextOutputSpeech)response.getOutputSpeech()).getText());
-    	
-    	assertNotNull(response.getCard());
-    	assertEquals(CARD_TITLE, ((SimpleCard)response.getCard()).getTitle());
-        assertEquals(CARD_CONTENT, ((SimpleCard)response.getCard()).getContent());
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void tellResponseEmptySpeechText() {
-    	ResponseFactory.tellResponse(EMPTY_TEXT, CARD_TITLE, CARD_CONTENT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void tellResponseEmptyCardTitle() {
-    	ResponseFactory.tellResponse(SPEECH_TEXT, EMPTY_TEXT, CARD_CONTENT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void tellResponseEmptyCardContent() {
-    	ResponseFactory.tellResponse(SPEECH_TEXT, CARD_TITLE, EMPTY_TEXT);
-    }
-    
-    @Test
-    public void createCloseReponse() {
-    	SpeechletResponse response = ResponseFactory.closeResponse(SPEECH_TEXT, CARD_TITLE, CARD_CONTENT);
-    	
-    	assertTrue(response.getShouldEndSession());
-    	assertNull(response.getReprompt());
-    	
-    	assertNotNull(response.getOutputSpeech());
-    	assertEquals(SPEECH_TEXT, ((PlainTextOutputSpeech)response.getOutputSpeech()).getText());
-    	
-    	assertNotNull(response.getCard());
-    	assertEquals(CARD_TITLE, ((SimpleCard)response.getCard()).getTitle());
-        assertEquals(CARD_CONTENT, ((SimpleCard)response.getCard()).getContent());
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void closeResponseEmptySpeechText() {
-    	ResponseFactory.closeResponse(EMPTY_TEXT, CARD_TITLE, CARD_CONTENT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void closeResponseEmptyCardTitle() {
-    	ResponseFactory.closeResponse(SPEECH_TEXT, EMPTY_TEXT, CARD_CONTENT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void closeResponseEmptyCardContent() {
-    	ResponseFactory.closeResponse(SPEECH_TEXT, CARD_TITLE, EMPTY_TEXT);
-    }
-    
-    @Test
-    public void createAskReponse() {
-    	SpeechletResponse response = ResponseFactory.askResponse(SPEECH_TEXT);
-    	
-    	assertFalse(response.getShouldEndSession());
-    	assertNotNull(response.getReprompt());
-    	assertEquals(SPEECH_TEXT, ((PlainTextOutputSpeech)((Reprompt)response.getReprompt()).getOutputSpeech()).getText());
-    	
     	
     	assertNotNull(response.getOutputSpeech());
     	assertEquals(SPEECH_TEXT, ((PlainTextOutputSpeech)response.getOutputSpeech()).getText());
@@ -149,14 +36,36 @@ public class ResponseFactoryTest {
     	assertNull(response.getCard());
     }
     
-    @Test(expected=IllegalArgumentException.class)
-    public void askResponseEmptySpeechText() {
-    	ResponseFactory.askResponse(EMPTY_TEXT);
+    @Test
+    public void withRepromptAndCard() {
+    	ResponseFactoryToken token = ResponseFactoryToken.builder()
+    		.outputSpeech(SPEECH_TEXT)
+    		.repromptSpeech(REPROMPT_TEXT)
+    		.cardTitle(CARD_TITLE)
+        	.cardContent(CARD_CONTENT)
+    		.build();
+    	SpeechletResponse response = ResponseFactory.from(token);
+    	
+    	assertFalse(response.getShouldEndSession());
+    	assertNotNull(response.getReprompt());
+    	assertEquals(REPROMPT_TEXT, ((PlainTextOutputSpeech)((Reprompt)response.getReprompt()).getOutputSpeech()).getText());
+    	
+    	
+    	assertNotNull(response.getOutputSpeech());
+    	assertEquals(SPEECH_TEXT, ((PlainTextOutputSpeech)response.getOutputSpeech()).getText());
+    	
+    	assertNotNull(response.getCard());
+    	assertEquals(CARD_TITLE, ((SimpleCard)response.getCard()).getTitle());
+        assertEquals(CARD_CONTENT, ((SimpleCard)response.getCard()).getContent());
     }
     
     @Test
-    public void createAskReponseWithRepompt() {
-    	SpeechletResponse response = ResponseFactory.askResponse(SPEECH_TEXT, REPROMPT_TEXT);
+    public void withRepompt() {
+    	ResponseFactoryToken token = ResponseFactoryToken.builder()
+        		.outputSpeech(SPEECH_TEXT)
+        		.repromptSpeech(REPROMPT_TEXT)
+        		.build();
+        SpeechletResponse response = ResponseFactory.from(token);
     	
     	assertFalse(response.getShouldEndSession());
     	assertNotNull(response.getReprompt());
@@ -169,46 +78,23 @@ public class ResponseFactoryTest {
     	assertNull(response.getCard());
     }
     
-    @Test(expected=IllegalArgumentException.class)
-    public void askResponseWithRepromptEmptySpeechText() {
-    	ResponseFactory.askResponse(EMPTY_TEXT, REPROMPT_TEXT);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void askResponseWithRepromptEmptyRepromptText() {
-    	ResponseFactory.askResponse(SPEECH_TEXT, EMPTY_TEXT);
-    }
-    
-    
     @Test
-    public void createAskReponseWithRepomptAndCard() {
-    	SpeechletResponse response = ResponseFactory.askResponse(SPEECH_TEXT, REPROMPT_TEXT, CARD_TITLE);
+    public void withCard() {
+    	ResponseFactoryToken token = ResponseFactoryToken.builder()
+        		.outputSpeech(SPEECH_TEXT)
+        		.cardTitle(CARD_TITLE)
+        		.cardContent(CARD_CONTENT)
+        		.build();
+    	SpeechletResponse response = ResponseFactory.from(token);
     	
     	assertFalse(response.getShouldEndSession());
-    	assertNotNull(response.getReprompt());
-    	assertEquals(REPROMPT_TEXT, ((PlainTextOutputSpeech)((Reprompt)response.getReprompt()).getOutputSpeech()).getText());
-    	
+    	assertNull(response.getReprompt());
     	
     	assertNotNull(response.getOutputSpeech());
     	assertEquals(SPEECH_TEXT, ((PlainTextOutputSpeech)response.getOutputSpeech()).getText());
     	
     	assertNotNull(response.getCard());
     	assertEquals(CARD_TITLE, ((SimpleCard)response.getCard()).getTitle());
-        assertEquals(SPEECH_TEXT, ((SimpleCard)response.getCard()).getContent());
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void askResponseWithRepromptAndCardEmptySpeechText() {
-    	ResponseFactory.askResponse(EMPTY_TEXT, REPROMPT_TEXT, CARD_TITLE);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void askResponseWithRepromptAndCardEmptyRepromptText() {
-    	ResponseFactory.askResponse(SPEECH_TEXT, EMPTY_TEXT, CARD_TITLE);
-    }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void askResponseWithRepromptAndCardEmptyCardTitle() {
-    	ResponseFactory.askResponse(SPEECH_TEXT, REPROMPT_TEXT, EMPTY_TEXT);
+        assertEquals(CARD_CONTENT, ((SimpleCard)response.getCard()).getContent());
     }
 }
